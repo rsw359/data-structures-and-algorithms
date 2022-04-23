84c6a77c6a7335f2321f424594e8870017b6c6f5
Roger Wells <rsw359@gmail.com>
2022-04-23 09:21:53 +0900

    all tests passed

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -4,12 +4,39 @@ class LinkedList:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        # Take in a response and return a string of the current nodes while traversing
+        response = ""
+        current = self.head
+        while current:
+            response += f"{{ {current.value} }} -> "
+            current = current.next
+        return response + "NULL"
+
+    def insert(self, value):
+        old_head = self.head
+        self.head = Node(value, self.head)
+        self.head.next = old_head
+
+    def includes(self, value):
+        # self.value = value
+        current = self.head
+
+        while current:
+            if current.value == value:
+                return True
+
+            current = current.next
+
+        return False
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
 
 
 class TargetError:
