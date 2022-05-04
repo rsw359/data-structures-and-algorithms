ff0216dd17d21939f247350ea64fe0de8d743d74
Roger Wells <rsw359@gmail.com>
2022-05-04 12:48:33 +0900

    finished code, added WB and updated readme

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,49 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.shelter = Queue()
+        self.dog = Queue()
+        self.cat = Queue()
+
+    def enqueue(self, animal):
+        if animal.type == 'cat':
+            self.cat.enqueue(animal)
+            self.shelter.enqueue(animal)
+        elif animal.type == 'dog':
+            self.dog.enqueue(animal)
+            self.shelter.enqueue(animal)
+
+    def dequeue(self, pref=None):
+
+        if pref is None:
+            any_animal = self.shelter.dequeue()
+            if any_animal.type == 'cat':
+                self.cat.dequeue()
+                return any_animal
+            elif any_animal.type == 'dog':
+                self.dog.dequeue()
+                return any_animal
+
+        if pref != 'cat' or pref != 'dog':
+            return None
+
+        elif pref == 'cat':
+            cat = self.cat.dequeue()
+            return cat
+
+        elif pref == 'dog':
+            dog = self.dog.dequeue()
+            return dog
 
 
 class Dog:
-    pass
+    def __init__(self, name=None):
+        self.type = 'dog'
+        self.name = name
 
 
 class Cat:
-    pass
+    def __init__(self, name=None):
+        self.type = 'cat'
+        self.name = name
