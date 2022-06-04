5c2619035220b033b0d094844346f5fe3a5e7e71
Roger Wells <rsw359@gmail.com>
2022-06-04 11:54:49 +0900

    basic graph implementation done

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -4,13 +4,42 @@ class Graph:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self._adjacency_list = {}
+        self.neighbors = []
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        vertex = Vertex(value)
+        self._adjacency_list[vertex] = []
+
+        return vertex
+
+    def size(self):
+
+        return len(self._adjacency_list)
+
+    def get_nodes(self):
+        return self._adjacency_list.keys()
+
+    def add_edge(self, start_vertex, end_vertex, weight=0):
+        if start_vertex not in self._adjacency_list or end_vertex not in self._adjacency_list:
+            raise KeyError()
+
+        edge = Edge(end_vertex, weight)
+
+        list_of_edges = self._adjacency_list[start_vertex]
+        list_of_edges.append(edge)
+
+    def get_neighbors(self, vertex):
+        return self._adjacency_list[vertex]
+
 
 
 class Vertex:
-    pass
+    def __init__(self, value):
+        self.value = value
+
+class Edge:
+    def __init__(self, vertex, weight):
+        self.vertex = vertex
+        self.weight = weight
+
--- python/tests/data_structures/test_graph.py
+++ python/tests/data_structures/test_graph.py
@@ -6,7 +6,6 @@ def test_exists():
     assert Graph
 
 
-@pytest.mark.skip("TODO")
 def test_add_node():
 
     graph = Graph()
@@ -18,7 +17,7 @@ def test_add_node():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+
 def test_size_empty():
 
     graph = Graph()
@@ -30,7 +29,7 @@ def test_size_empty():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+
 def test_size():
 
     graph = Graph()
@@ -44,7 +43,7 @@ def test_size():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+
 def test_add_edge():
     g = Graph()
     apple = g.add_node("apple")
@@ -93,7 +92,7 @@ def test_add_edge_interloper_end():
         graph.add_edge(start, end)
 
 
-@pytest.mark.skip("TODO")
+
 def test_get_nodes():
 
     graph = Graph()
@@ -111,7 +110,7 @@ def test_get_nodes():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+
 def test_get_neighbors():
 
     graph = Graph()
